fb590bece27103c69e64f500087ef770091a0aed
Claudia <clau.lertora@gmail.com>
2022-11-26 22:31:41 +0100

    create simple express app with a get

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world! Today is "+Date.now())
+const express = require('express');
+const app = express();
+ 
+//Configurations
+app.set('port', 3000);
+app.set('json spaces', 2)
+ 
+// API
+app.get('/', (req, res) => {    
+    res.json(
+        {
+            "Title": "Hola mundo",
+            "Author": "Claudia",
+        }
+    );
+})
+ 
+//Start server
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
